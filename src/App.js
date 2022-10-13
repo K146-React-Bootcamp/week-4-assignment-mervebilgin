713817a29c935ca6a8517dfb27cdb7ff1c2f9541
bilginmrv <mrvblgn44@gmail.com>
2022-10-13 22:16:18 +0300

    Create context structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import Navbar from "./components/Navbar/Navbar";
 import "./App.css";
 
+import { useContext } from "react";
+import { themeContext } from "./Context";
+
 function App() {
-  
+  const theme = useContext(themeContext);
+  const darkMode = theme.state.darkMode;
   return (
-    <div className="App">
+    <div
+      className="App"
+      style={{
+        background: darkMode ? "black" : "",
+        color: darkMode ? "white" : "",
+      }}>
+
+
       <Navbar/>
+    
     </div>
   );
 }
