408f1b8965d0fde04096f3c0d8968ed99046ec34
bilginmrv <mrvblgn44@gmail.com>
2022-10-14 10:35:46 +0300

    Create Howitworks page left section

--- src/components/Howitworks/Howitworks.jsx
+++ src/components/Howitworks/Howitworks.jsx
@@ -1,12 +1,23 @@
 import React, { useContext } from "react";
 import "./Howitworks.css";
+import Card from "../Card/Card";
+import HeartEmoji from "../../img/heartemoji.png";
+import Glasses from "../../img/glasses.png";
+import Humble from "../../img/humble.png";
 import { themeContext } from "../../Context";
+import { motion } from "framer-motion";
 import AcademicCalendar from './academicCalendar.pdf';
 
 const Howitworks = () => {
   // context
   const theme = useContext(themeContext);
   const darkMode = theme.state.darkMode;
+
+  // transition
+  const transition = {
+    duration: 1,
+    type: "spring",
+  };
 
   return (
     <div className="howitworks" id="howitworks">
@@ -26,10 +37,50 @@ const Howitworks = () => {
         <div className="blur s-blur1" style={{ background: "#ABF1FF94" }}></div>
       </div>
       {/* right */}
-
       <div className="cards">
-        Right Section
-      
+        {/* first card */}
+        <motion.div
+          initial={{ left: "25rem" }}
+          whileInView={{ left: "14rem" }}
+          transition={transition}
+        >
+          <Card
+            emoji={HeartEmoji}
+            heading={"1"}
+            detail={"Öğrenciler sorular gönderebilir ve bu sorulara verilen yanıtları düzenlemek için iş birliği yapabilir."}
+          />
+        </motion.div>
+        {/* second card */}
+        <motion.div
+          initial={{ left: "-11rem", top: "12rem" }}
+          whileInView={{ left: "-4rem" }}
+          transition={transition}
+        >
+          <Card
+            emoji={Glasses}
+            heading={"2"}
+            detail={"Eğitmenler ayrıca soruları yanıtlayabilir, öğrenci yanıtlarını onaylayabilir."}
+          />
+        </motion.div>
+        {/* 3rd */}
+        <motion.div
+          initial={{ top: "19rem", left: "25rem" }}
+          whileInView={{ left: "12rem" }}
+          transition={transition}
+        >
+          <Card
+            emoji={Humble}
+            heading={"3"}
+            detail={
+              "Eğitmenler yayınlanan herhangi bir içeriği düzenleyebilir veya silebilir."
+            }
+            color="rgba(252, 166, 31, 0.45)"
+          />
+        </motion.div>
+        <div
+          className="blur s-blur2"
+          style={{ background: "var(--purple)" }}
+        ></div>
       </div>
     </div>
     
